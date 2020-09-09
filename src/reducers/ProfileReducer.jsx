cd890432d9b39d16f0241f09624fbf05302aa6f1
Phuti Mokwena <phuti.mokwena@liberty.co.za>
2020-09-09 13:01:03 +0200

    Profile management project

---
+++ src/reducers/ProfileReducer.jsx
@@ -0,0 +1,34 @@
+import {
+    DATA_LOADED,
+    DATA_FAILED,
+    PROFILE_LOADED,
+  } from "../ProfileType";
+
+  export default (
+    state = {
+      loadingError: false,
+      profiles: [],
+      profileInfo: {}
+    },
+    action
+  ) => {
+    switch (action.type) {
+      case DATA_LOADED:
+        return {
+          profiles: action.profiles,
+          loadingError: false
+        };
+        case PROFILE_LOADED:
+        return {
+          profileInfo: action.profileInfo,
+          loadingError: false
+        };
+      case DATA_FAILED:
+        return {
+          ...state,
+          loadingError: true
+        };
+      default:
+        return state;
+    }
+  };
