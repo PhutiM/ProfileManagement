e6cc7809732474c3a8ff6441dd017221fe821278
Phuti Mokwena <phuti.mokwena@liberty.co.za>
2020-09-09 13:19:14 +0200

    Updated Package.json

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,9 +1,6 @@
-import React, {useState, useEffect} from 'react'
-import { makeStyles, withStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import { Grid, Typography, MenuItem, Select, TextField } from '@material-ui/core'
-import MuiTableCell from "@material-ui/core/TableCell"
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Grid, Typography, TextField } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import { useHistory } from "react-router-dom"
 
